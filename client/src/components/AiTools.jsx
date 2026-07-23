e243290ff997e6dfe95b8fc8b8d19375660cf875
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-07-23 15:39:14 +0500

    feat: testimonial component added using prebuiltui lib

--- client/src/components/AiTools.jsx
+++ client/src/components/AiTools.jsx
@@ -8,7 +8,7 @@ const AiTools = () => {
   const { user } = useUser();
 
   return (
-    <div className="px-4 sm:px-20 xl:px-32 my-24">
+    <div className="px-4 sm:px-20 xl:px-32 mt-10">
       <div className="text-center">
         <h2 className="text-slate-700 text-[42px] font-semibold">
           Powerful AI tools
